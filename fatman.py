cc32523c2a7e45f7fd9fe602a3cc3384aaa20021
Dibyendu Das <43660480+Dibyendu1000@users.noreply.github.com>
2019-01-19 09:53:29 +0530

    add social foci

--- fatman.py
+++ fatman.py
@@ -7,8 +7,8 @@ def create_graph():
     G.add_nodes_from(range(1,101))
     return G
 
-def visualize(G,labeldict,nsize):
-    nx.draw(G,labels=labeldict,node_size=nsize)
+def visualize(G,labeldict,nsize,color):
+    nx.draw(G,labels=labeldict,node_size=nsize, node_color=color)
     plt.show()
 
 def assign_bmi(G):
@@ -25,12 +25,60 @@ def get_labels(G):
 def get_size(G):
     array1=[]
     for each in G.nodes():
-        array1.append(G.node[each]['name']*20)
+        if(G.node[each]['type']=='person'):
+            array1.append(G.node[each]['name']*20)
+        else:
+            array1.append(1000)
     return array1
+
+def add_foci_nodes(G):
+    n=G.number_of_nodes()
+    i=n+1
+    foci_nodes=['gym','eatout','movie_club','karate_club','yoga_club']
+    for j in range(0,5):
+        G.add_node(i)
+        G.node[i]['name']=foci_nodes[j]
+        G.node[i]['type']='foci'
+        i+=1
+
+def get_colors(G):
+    c=[]
+    for each in G.nodes():
+        if (G.node[each]['type']=='person'):
+            c.append('blue')
+        else:
+            c.append('red')
+    return c
+
+def get_foci_nodes():
+    f=[]
+    for each in G.nodes():
+        if (G.node[each]['type']=='foci'):
+            f.append(each)
+    return f
+
+def get_person_nodes():
+    p=[]
+    for each in G.nodes():
+        if (G.node[each]['type']=='person'):
+            p.append(each)
+    return p
+
+def add_foci_edges():
+    foci_nodes=get_foci_nodes()
+    people_nodes=get_person_nodes()
+    for each in people_nodes:
+        r=random.choice(foci_nodes)
+        G.add_edge(each,r)
+    
+        
+    
 
 G=create_graph()
 assign_bmi(G)
+add_foci_nodes(G)
+add_foci_edges()
 labeldict=get_labels(G)
-visualize(G,labeldict,get_size(G))
+visualize(G,labeldict,get_size(G),get_colors(G))
 assign_bmi(G)
 
